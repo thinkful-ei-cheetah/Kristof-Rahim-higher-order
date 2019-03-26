fe9096846e544f9ef476260b7295c7eb1871abdf
rahmor <rahmor@yahoo.com>
2019-03-26 12:42:49 -0400

    add arg1drills.js

---
+++ arg1drill.js
@@ -0,0 +1,17 @@
+"use strict";
+
+function repeat(callback, n) {
+  for (let i = 0; i < n; i++) {
+    callback();
+  }
+}
+
+function hello() {
+  console.log("Hello world");
+}
+function goodbye() {
+  console.log("Goodbye world");
+}
+
+repeat(hello, 5);
+repeat(goodbye, 5);
